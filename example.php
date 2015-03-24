e5cc330724dda6584b439fee51021fa2a4128345
thunder <thunder@debianserver.thunderace.fr>
2015-03-24 09:42:52 +0100

    image links updated

--- example.php
+++ example.php
@@ -11,65 +11,66 @@ $deviceIds[] = "MyFirstGCMDeviceID";
 // samples
 // standard message
 $result = newtifryProPush(	$apikey,
-							$deviceIds, 
-							"Test message 1", 
-							"Normal", 
-							"Hello from NewtifryPro", 
-							1, 
-							"https://newtifry.appspot.com", 
-							"http://www.newtifry.org/test_newtifry.jpg", 
-							-1,	// speak 
-							false, 	// noCache
-							0, 	// state : default
-							-1); 	// notify
+              							$deviceIds, 
+              							"Test message 1", 
+              							"Normal", 
+              							"Hello from NewtifryPro", 
+              							1, 
+              							"https://newtifry.appspot.com", 
+              							"http://upload.wikimedia.org/wikipedia/commons/b/b5/PA120016.JPG", 
+              							-1,	// speak 
+              							false, 	// noCache
+              							0, 	// state : default
+              							-1); 	// notify
 print_r($result);
 // sticky message
 $result = newtifryProPush(	$apikey,
-							$deviceIds, 
-							"Test message 2", 
-							"Sticky", 
-							"Hello from NewtifryPro", 
-							1, 
-							"https://newtifry.appspot.com", 
-							"http://www.newtifry.org/test_newtifry.jpg", 
-							-1,	// speak 
-							false, 	// noCache
-							1, 	// state : sticky
-							-1); 	// notify
+              							$deviceIds, 
+              							"Test message 2", 
+              							"Sticky", 
+              							"Hello from NewtifryPro", 
+              							1, 
+              							"https://newtifry.appspot.com", 
+              							"http://upload.wikimedia.org/wikipedia/commons/b/b5/PA120016.JPG", 
+              							-1,	// speak 
+              							false, 	// noCache
+              							1, 	// state : sticky
+              							-1); 	// notify
 print_r($result);
 // locked message
 $result = newtifryProPush(	$apikey,
-							$deviceIds, 
-							"Test message 3", 
-							"Locked", 
-							"Hello from NewtifryPro", 
-							1, 
-							"https://newtifry.appspot.com", 
-							"http://www.newtifry.org/test_newtifry.jpg", 
-							-1,	// speak 
-							false, 	// noCache
-							2, 	// state : locked
-							-1); 	// notify
+              							$deviceIds, 
+              							"Test message 3", 
+              							"Locked", 
+              							"Hello from NewtifryPro", 
+              							1, 
+              							"https://newtifry.appspot.com", 
+              							"http://upload.wikimedia.org/wikipedia/commons/b/b5/PA120016.JPG", 
+              							-1,	// speak 
+              							false, 	// noCache
+              							2, 	// state : locked
+              							-1); 	// notify
 print_r($result);
 
 // with 5 images
 $images = array(
-    "http://www.newtifry.org/test_newtifry1.jpg",
-    "http://www.newtifry.org/test_newtifry2.png",
-    "http://www.newtifry.org/test_newtifry3.jpg",
-    "http://www.newtifry.org/test_newtifry4.jpg",
-    "http://www.newtifry.org/test_newtifry5.jpg");
+  "http://upload.wikimedia.org/wikipedia/commons/b/b5/PA120016.JPG",
+  "http://upload.wikimedia.org/wikipedia/commons/c/c3/The_Blue_Bell_-_geograph.org.uk_-_163687.jpg",
+  "http://upload.wikimedia.org/wikipedia/commons/b/b3/Trassenheide%2C_Die_Welt_steht_Kopf.jpg",
+  "http://upload.wikimedia.org/wikipedia/commons/2/20/Bachorza%2C_dw%C3%B3r_A-259.jpg",
+  "http://upload.wikimedia.org/wikipedia/commons/2/20/Interior_of_log_house.jpg");
+
 $result = newtifryProPush(  $apikey,
-							$deviceIds, 
-							"Test message 4", 
-							"locked 5 images", 
-							"Hello from NewtifryPro", 
-							1, 
-							"https://newtifry.appspot.com", 
-							$images, 
-							-1,	// speak 
-							false, 	// noCache
-							2, 	// state : locked
-							-1); 	// notify
+							              $deviceIds, 
+							              "Test message 4", 
+							              "locked 5 images", 
+							              "Hello from NewtifryPro", 
+							              1, 
+							              "https://newtifry.appspot.com", 
+							              $images, 
+							              -1,	// speak 
+							              false, 	// noCache
+							              2, 	// state : locked
+							              -1); 	// notify
     
 ?>
